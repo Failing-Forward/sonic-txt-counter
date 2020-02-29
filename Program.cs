2fc3c960c0843eadae774d973004caefe208d189
Dmitry Mikhaltsov <45086516+bqio@users.noreply.github.com>
2020-02-29 08:53:13 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace sonictxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                return;
+            }
+
+            string text = File.ReadAllText(args[0]);
+            string[] arr = text.Split("\r\n\r\n");
+
+            string data = "";
+            for (int i = 0; i < arr.Length; i++) {
+                data += $"[{i}]\n{arr[i]}\n";
+            }
+            File.WriteAllText(Path.GetFileName(args[0]) + "_count.txt", data);
+        }
+    }
+}
